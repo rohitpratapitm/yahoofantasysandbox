68484fff1df3c07e0443ba13228d2f4f152f8ad5
rohitpratapitm <rohitpratapitm@gmail.com>
2017-11-23 16:47:17 -0500

    updated path

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.set('view engine', 'pug');
 app.yf = new YahooFantasy(APP_KEY, APP_SECRET);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
